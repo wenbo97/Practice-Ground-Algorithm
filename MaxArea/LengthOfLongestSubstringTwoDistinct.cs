01c3ea14e6a935761d81ead34dadbcd6fed2eb8e
Wenbo Zhao (Shanghai Wicresoft Co Ltd) (from Dev Box) <v-wangjunf@microsoft.com>
2025-12-17 17:43:44 +0800

    Update leetcode 159

---
+++ MaxArea/LengthOfLongestSubstringTwoDistinct.cs
@@ -0,0 +1,25 @@
+﻿namespace MaxArea;
+
+public class LengthOfLongestSubstringTwoDistinct
+{
+    public static int LengthOfLongestSubstringTwoDistinctBase(string s)
+    {
+        int maxLen = 0;
+        int slow = 0;
+        int fast = 0;
+        Dictionary<char, int> map = new Dictionary<char, int>();
+        while (fast < s.Length)
+        {
+            if (!map.ContainsKey(s[fast]))
+            {
+                if (map.Keys.Count < 3)
+                {
+                    map[s[fast]] = map.GetValueOrDefault(s[fast], 0) + 1;
+                }
+
+            }
+        }
+
+        return maxLen;
+    }
+}
