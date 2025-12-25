bd12475136969440662cc642e26a5443f16cca1f
Wenbo Zhao (Shanghai Wicresoft Co Ltd) (from Dev Box) <v-wangjunf@microsoft.com>
2025-12-25 17:52:49 +0800

    Update leetcode 340

---
+++ MaxArea/LengthOfLongestSubstringKDistinct.cs
@@ -0,0 +1,41 @@
+﻿namespace MaxArea;
+
+public class LengthOfLongestSubstringKDistinct
+{
+    public static int LengthOfLongestSubstringKDistinctBase(string s, int k)
+    {
+        int maxLen = 0;
+        int slow = 0;
+        int fast = 0;
+
+        Dictionary<char, int> map = new();
+
+        while (fast < s.Length)
+        {
+            char c = s[fast];
+            if (map.ContainsKey(c))
+            {
+                map[c] = map[c] + 1;
+            }
+            else
+            {
+                map[c] = 1;
+            }
+
+            while (map.Keys.Count > k)
+            {
+                char charFromSlowPos = s[slow];
+                map[charFromSlowPos]--;
+                if (map[charFromSlowPos] == 0)
+                {
+                    map.Remove(charFromSlowPos);
+                }
+                slow++;
+            }
+
+            maxLen = Math.Max(maxLen, fast - slow + 1);
+            fast++;
+        }
+        return maxLen;
+    }
+}
