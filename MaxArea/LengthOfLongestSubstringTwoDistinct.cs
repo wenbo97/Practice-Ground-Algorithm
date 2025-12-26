b27e1e314b3929cb806f0c70a00cf74d76adfd9a
Wenbo Zhao (Shanghai Wicresoft Co Ltd) (from Dev Box) <v-wangjunf@microsoft.com>
2025-12-26 17:23:55 +0800

    Update array solution for 159

--- MaxArea/LengthOfLongestSubstringTwoDistinct.cs
+++ MaxArea/LengthOfLongestSubstringTwoDistinct.cs
@@ -21,7 +21,7 @@ public class LengthOfLongestSubstringTwoDistinct
                 map[c] = 1;
             }
 
-            while(map.Keys.Count > 2)
+            while (map.Keys.Count > 2)
             {
                 map[s[slow]]--;
                 if (map[s[slow]] == 0)
@@ -35,6 +35,46 @@ public class LengthOfLongestSubstringTwoDistinct
             fast++;
         }
 
+        return maxLen;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="s">Target string</param>
+    /// <returns>Maximum of the substring</returns>
+    public static int LengthOfLongestSubstringTwoDistinctViaArray(string s)
+    {
+        int maxLen = 0;
+        int slow = 0;
+        int fast = 0;
+
+        int[] map = new int[128];
+        Array.Fill(map, 0);
+        int distinctCharCount = 0;
+
+        while (fast < s.Length)
+        {
+            char fastChar = s[fast];
+            if (map[fastChar] == 0)
+            {
+                distinctCharCount++;
+            }
+            map[fastChar]++;
+
+            while (distinctCharCount > 2)
+            {
+                char slowChar = s[slow];
+                map[slowChar]--;
+                if (map[slowChar] == 0)
+                {
+                    distinctCharCount--;
+                }
+                slow++;
+            }
+            fast++;
+            maxLen = Math.Max(maxLen, fast - slow);
+        }
         return maxLen;
     }
 }
--- docs/Algorithm/Leetcode Top 100/159 Length Of Longest Substring Two Distinct.md
+++ docs/Algorithm/Leetcode Top 100/159 Length Of Longest Substring Two Distinct.md
@@ -1,4 +1,4 @@
-[Link]()
+[Link](https://leetcode.cn/problems/longest-substring-with-at-most-two-distinct-characters/)
 
 But the sliding window rule follows the **First In, First Out**(FIFO) principle, just like a queue.
 
@@ -36,4 +36,44 @@ Use map to save sliding window
         }
 
         return maxLen;
+```
+
+If use an array to save the char counts in a valid window, the unique char counts are required manually maintain.
+
+```csharp
+    public static int LengthOfLongestSubstringTwoDistinctViaArray(string s)
+    {
+        int maxLen = 0;
+        int slow = 0;
+        int fast = 0;
+
+        int[] map = new int[128];
+        Array.Fill(map, 0);
+        int distinctCharCount = 0;
+
+        while (fast < s.Length)
+        {
+            char fastChar = s[fast];
+            // Fast pos walk to an postion of the array
+            if (map[fastChar] == 0)
+            {
+                distinctCharCount++;
+            }
+            map[fastChar]++;
+
+            while (distinctCharCount > 2)
+            {
+                char slowChar = s[slow];
+                map[slowChar]--;
+                if (map[slowChar] == 0)
+                {
+                    distinctCharCount--;
+                }
+                slow++;
+            }
+            fast++;
+            maxLen = Math.Max(maxLen, fast - slow);
+        }
+        return maxLen;
+    }
 ```
