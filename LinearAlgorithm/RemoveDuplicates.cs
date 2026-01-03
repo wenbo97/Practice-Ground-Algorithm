ef8408371c3c8abe5750e9236a60a38ba398b1ca
Wenbo Zhao <179686042+wenbo97@users.noreply.github.com>
2026-01-03 19:56:14 +0800

    Update leetcode 840

---
+++ LinearAlgorithm/RemoveDuplicates.cs
@@ -0,0 +1,21 @@
+﻿namespace LinearAlgorithm;
+
+public class RemoveDuplicates
+{
+    public static int RemoveDuplicatesBase(int[] nums)
+    {
+        int slow = 1;
+        int fast = 1;
+
+        while (fast < nums.Length)
+        {
+            if (nums[fast] != nums[fast - 1])
+            {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
+        }
+        return slow;
+    }
+}
